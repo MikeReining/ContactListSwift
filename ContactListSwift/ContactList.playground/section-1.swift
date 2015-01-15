244c08b4c847ae558742ab637c84b3a72b0c77de
Mike Reining <emailmike@gmail.com>
2015-01-15 15:50:49 -0800

    Started Implementing functions in Contact List app

--- ContactListSwift/ContactList.playground/section-1.swift
+++ ContactListSwift/ContactList.playground/section-1.swift
@@ -3,17 +3,14 @@
 
 var str = "Hello, playground"
 
+// MARK: - Data Model
+
 class AddressBook {
     var contacts = [Contact]()
     init() {
         
     }
     
-    func createNewContactWithName(name: String, email: String) -> Contact {
-        var newContact = Contact(name: name, email: email)
-        self.contacts.append(newContact)
-        return newContact
-    }
 }
 
 class Contact {
@@ -25,11 +22,71 @@ class Contact {
     }
 }
 
+enum Menu {
+    case NewContact, ListContacts, DeleteAll, Quit
+    func printMenuOptions() -> String {
+        switch self {
+        case .NewContact:
+            return "Create a New Contact"
+        case .ListContacts:
+            return "List all Contacts"
+        case .DeleteAll:
+            return "Delete Address Book"
+        case .Quit:
+            return "Quit application"
+        }
+    }
+}
+
+
+// MARK: Functions
+
+func createNewContactWithName(name: String, #email: String, addressBook: AddressBook) {
+    var newContact = Contact(name: name, email: email)
+    addressBook.contacts.append(newContact)
+}
+
+func deleteAllContacts() {
+    addressBook.contacts.removeAll(keepCapacity: false)
+}
+
+
+func userSelected(userSelection: Menu) {
+    switch userSelection {
+    case .DeleteAll:
+        deleteAllContacts()
+    case .NewContact:
+        createNewContactWithName("NEW CONTACT", email: "EMAIL", addressBook)
+        
+        //    case .ListContacts:
+    case .Quit:
+        println("Good Bye!  Thanks for checking out my app")
+    default:
+        println("Error - This line should never be called")
+    }
+}
+
+// MARK: Test User Options
+
+
+
+
+
 
 let addressBook = AddressBook()
 
-addressBook.createNewContactWithName("Mike", email: "email@gmail.com")
+createNewContactWithName("Mike", email: "email@gmail.com", addressBook)
+createNewContactWithName("Sonja", email: "", addressBook)
+createNewContactWithName("Michelle", email: "michelle@gmail.com", addressBook)
 
 addressBook.contacts.count
 
 
+
+
+
+// Task 1 - Delete All Contacts from Address Book
+
+
+
+addressBook.contacts
